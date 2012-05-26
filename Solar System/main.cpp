9706cc997cc0f4223ff48f411e6d5e63cb966a38
Helena Moreno Chacon <helenatxu@Helenas-MacBook-Pro.local>
2012-05-25 20:34:23 -0400

    Initial Commit

---
+++ Solar System/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Solar System
+//
+//  Created by Helena Moreno Chacon on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
